304522ccafa560faaac707fc33bc4698f739feea
DanielRosso <daniel.rosso@hotmail.com>
2016-01-04 14:54:58 +0100

    refactored the usage of the $q service add dps number to hero portrait

--- client/app/services/hero.service.js
+++ client/app/services/hero.service.js
@@ -9,82 +9,24 @@
 
     function heroService($http, $q, urlService) {
         var service = {
-            getHeroModel: getHeroModel,
-            calculateDPS: calculateDPS,
-            loadHeroesData: loadHeroesData,
-            loadHeroesData2: loadHeroesData2
+            loadHeroesData: loadHeroesData
         };
 
         return service;
 
         /////////////////
-
-        //function getHeroModel(rawData, heroStats, items) {            
-        function getHeroModel(data) {
-            var rawData = data.data;
-            var heroStats = data.data.stats;
-            var items = data.data.items;
-            
-            //            durch alle items loopen. ich brauche von allen elemental dmg und so zeugs.$anchorScroll
-            //            http://stackoverflow.com/questions/21024411/angular-q-how-to-chain-multiple-promises-within-and-after-a-for-loop
-
-            var defer = $q.defer();
-            var promises = [];
-
-            function createHeroModel(e) {
-                //model erstellen
-                var items = [];
-                var heroModel = {};
-
-                angular.forEach(e, function (element) {
-                    var itemModel = {};
-                    itemModel.item = element.config.item;
-                    itemModel.stats = element.data;
-                    items.push(itemModel);
-                });
-
-                heroModel.stats = heroStats;
-                heroModel.items = items;
-                heroModel.rawData = e[0].config.rawData;
-
-                defer.resolve(heroModel);
-            }
-
-            function loadItem(url, heroStats, item, rawData) {
-                return $http.jsonp(url, {
-                    //der zweite param wird im config obj gespeichert. welche das element ist von den einzelnen promises.
-                    //siehe createHeroModel(e) e=element. damit behalte ich das urspürngliche item mit dem ergebniss zussammen
-                    item: item,
-                    heroStats: heroStats,
-                    rawData: rawData
-                });
-            }
-
-            angular.forEach(items, function (item, key) {
-                var url = urlService.getUrlForItem(item.tooltipParams);
-                item.slot = key;
-                promises.push(loadItem(url, heroStats, item, rawData));
-            });
-
-            $q.all(promises).then(createHeroModel);
-
-            return defer.promise;
-        }
-
-        function loadHeroesData2(heroList, battleNetTag) {
-            return $q.all(heroList.map(loadHeroData2, battleNetTag))
-                .then(function (result) {
-                    var x = result;
-                    var hero = hero;
-                });
+        function loadHeroesData(heroList, battleNetTag) {
+            return $q.all(heroList.map(loadHeroData2, battleNetTag)); //map: auf jedes item in der liste wird die funktion angewendet
         }
 
         function loadHeroData2(hero) {
-            var battleNetTag = this;
+            var battleNetTag = this; //zweiter param von loadHeroesData
             var url = urlService.getUrlForHero(hero.id, battleNetTag);
             return $http.jsonp(url)
                 .then(function (result) {
                     var itemList = [];
+                                        
+                    //braucht es weil result.items keine arraylist ist
                     Object.keys(result.data.items).forEach(function (key) {
                         var item = {};
                         item.slot = key;
@@ -92,62 +34,36 @@
                         itemList.push(item);
                     });
 
-                    return $q.all(itemList.map(loadItemData2))
+                    return $q.all(itemList.map(loadItemData))
+                        .then(function (itemlist) {
+                            var hero = result.data;
+                        
+                            //die ganzen stats den items zuweisen
+                            Object.keys(hero.items).forEach(function (key) {
+                                for (var i = 0; i < itemlist.length; i++) {
+                                    var item = itemlist[i];
+                                    if (key == item.config.item.slot) {
+                                        hero.items[key].stats = item.data;
+                                        itemlist.splice(i, 1); //removen von liste
+                                        break;
+                                    }
+                                }
+                            });
+
+                            hero.dpsModel = calculateDPS(hero);
+
+                            return hero;
+                        });
                 });
         }
         
-        /**
-         * item objekt { slot: 'head', data: data}
-         */
-        function loadItemData2(item, index) {
+        function loadItemData(item, index) {
             var url = urlService.getUrlForItem(item.data.tooltipParams);
             //der zweite param ist ein config obj.
             return $http.jsonp(url, {
                 item: item,
             });
         }
-
-        /*
-                function loadHeroesData(heroList, battleNetTag) {
-                    var defer = $q.defer();
-                    var promises = [];
-        
-                    function createHeroList(e) {
-                        var hero1 = {};
-                        hero1.dps = 100;
-        
-                        var hero2 = {}
-                        hero2.dps = 200;
-        
-                        var hero3 = {}
-                        hero2.dps = 300;
-        
-        
-                        var model = { hero1, hero2, hero3 };
-        
-                        defer.resolve(model);
-                    }
-        
-                    function loadHeroData(url) {
-                        return $http.jsonp(url)
-                            .then(function (data) {
-                                getHeroModel(data.data, data.data.stats, data.data.items)
-                                    .then(function (data) {
-                                        data.dps = calculateDPS(data);
-                                    });
-                            });
-                    }
-        
-                    angular.forEach(heroList, function (hero, index) {
-                        var url = urlService.getUrlForHero(hero.id, battleNetTag);
-                        promises.push(loadHeroData(url));
-                    });
-        
-                    $q.all(promises).then(createHeroList);
-        
-                    return defer.promise;
-                }
-                */
 
         function calculateDPS(heroModel) {
             var dpsModel = {
@@ -171,9 +87,9 @@
             var critChance = heroModel.stats.critChance;
             var critDamage = heroModel.stats.critDamage;
 
-            angular.forEach(heroModel.items, function (e) {
-                if (e.item.slot == "mainHand") {
-                    averageWeaponDamage = e.stats.dps.min;
+            angular.forEach(heroModel.items, function (value, key) {
+                if (key == "mainHand") {
+                    averageWeaponDamage = value.stats.dps.min;
                 }
             });
             //        DPS = (Sum Average Weapon Damage)*AS*(10*%Crit)*(10*%Crit Damage)
